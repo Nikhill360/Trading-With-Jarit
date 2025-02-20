1465b499d8ceffdfe16a8f50e885d985c45b117a
Nikhill360 <dhrunikhil@gmail.com>
2025-02-20 12:18:12 +0530

    mobilebox

--- src/app/autotrading-bot/page.js
+++ src/app/autotrading-bot/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function autotradingbot() {
     return (
         <>
-            <div className="bg-[#024430] w-full h-auto py-10">
+            {/* <div className="bg-[#024430] w-full h-auto py-10">
 
                 <div className="flex flex-col md:flex-row justify-between gap-5 md:gap-10 px-5 md:px-20 py-11 items-center">
 
@@ -101,7 +101,38 @@ export default function autotradingbot() {
 
 
 
-            </div>
+            </div> */}
+
+ 
+                    <div className="allSideShadow  h-screen w-screen flex flex-row bg-[#032B1F]  p-5 ">
+
+                        
+
+                            <div className="w-1/2 h-full"> 
+                            <img src="/images/mobile.jpg"  alt="Mobile View" />
+                            </div>
+
+                            <div className="w-1/2 h-full flex flex-col gap-10  text-3xl  p-8" > 
+                                <h1>🟢 Make TWJ LIVE SIGNALS SOFTWARE your Most Valuable Asset !!</h1>
+                                <h1>
+                                ⚡ For every trader who actively trades in the binary options market & spends hours of time in analyzing charts, drawing price action levels- Support, Resistance, Trendline etc which can be done in just a single click with our software. On top of that you get real-time signal alerts with arrows for all live and otc pairs.
+                                </h1>
+                                <h1>🟢 Make TWJ LIVE SIGNALS SOFTWARE your Most Valuable Asset !!</h1>
+
+
+
+                            </div>
+
+                       
+
+                    </div>
+
+
+
+
+
+
+
 
 
 
---
+++ src/components/mobilebox.jsx
@@ -0,0 +1,30 @@
+export default function mobilebox() {
+    return (
+
+        <>
+            <div className="allSideShadow  h-screen w-screen flex flex-row bg-[#032B1F]  p-5 ">
+
+
+
+                <div className="w-1/2 h-full">
+                    <img src="/images/mobile.jpg" alt="Mobile View" />
+                </div>
+
+                <div className="w-1/2 h-full flex flex-col gap-10  text-3xl  p-8" >
+                    <h1>🟢 Make TWJ LIVE SIGNALS SOFTWARE your Most Valuable Asset !!</h1>
+                    <h1>
+                        ⚡ For every trader who actively trades in the binary options market & spends hours of time in analyzing charts, drawing price action levels- Support, Resistance, Trendline etc which can be done in just a single click with our software. On top of that you get real-time signal alerts with arrows for all live and otc pairs.
+                    </h1>
+                    <h1>🟢 Make TWJ LIVE SIGNALS SOFTWARE your Most Valuable Asset !!</h1>
+
+
+
+                </div>
+
+
+
+            </div>
+
+        </>
+    );
+}
